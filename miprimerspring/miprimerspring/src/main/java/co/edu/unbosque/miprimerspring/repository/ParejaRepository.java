e8e88e541d645b3f2c8b6e5f98fa8f1225064c43
OtherSpanish <juankamome95@gmail.com>
2026-04-08 12:35:48 -0500

    Spring proyect Juan added
    I addeed my springboot project

---
+++ miprimerspring/miprimerspring/src/main/java/co/edu/unbosque/miprimerspring/repository/ParejaRepository.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.miprimerspring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import co.edu.unbosque.miprimerspring.entity.Pareja;
+
+public interface ParejaRepository extends CrudRepository<Pareja, Long> {
+	
+}
